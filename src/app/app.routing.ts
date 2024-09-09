1b50957f7d812943e2b615742d5b01da1444921d
vasuyadav1220 <mayankyadav1203@gmail.com>
2024-09-09 10:28:31 +0530

    user table added

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,17 @@
 import { Routes } from '@angular/router';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { LoginComponent } from './login/login.component';
 
 export const AppRoutes: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
+    redirectTo: 'login',
     pathMatch: 'full',
+  },
+  {
+    path:'login',
+    component:LoginComponent
   }, {
     path: '',
     component: AdminLayoutComponent,
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -10,11 +10,12 @@ export interface RouteInfo {
 
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard',     title: 'Status',         icon:'nc-check-2',       class: '' },
+    { path: '/users',         title: 'Users',        icon:'nc-single-02',    class: '' },
     { path: '/icons',         title: 'Alerts',             icon:'nc-bell-55',    class: '' },
     { path: '/maps',          title: 'Privacy',              icon:'nc-key-25',      class: '' },
     { path: '/notifications', title: 'Devices',     icon:'nc-mobile',    class: '' },
     { path: '/user',          title: 'Performance',      icon:'nc-chart-bar-32',  class: '' },
-    { path: '/table',         title: 'Reports',        icon:'nc-paper-2',    class: '' },
+    { path: '/table',         title: 'Reports',        icon:'nc-paper',    class: '' },
     { path: '/typography',    title: 'Account',        icon:'nc-circle-09', class: '' },
     { path: '/upgrade',       title: 'More',    icon:'nc-bullet-list-67',  class: 'active-pro' },
 ];
